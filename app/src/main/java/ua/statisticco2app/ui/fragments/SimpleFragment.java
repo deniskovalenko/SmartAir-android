b2b46becfb27b4ea548f8d8c26bec1f9a8e53e6c
Sovgyr Dmitry <dimdron@gmail.com>
2015-03-17 23:44:42 +0200

    added swipe refresh layout

--- app/src/main/java/ua/statisticco2app/ui/fragments/SimpleFragment.java
+++ app/src/main/java/ua/statisticco2app/ui/fragments/SimpleFragment.java
@@ -1,5 +1,6 @@
 package ua.statisticco2app.ui.fragments;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -14,7 +15,7 @@ import ua.statisticco2app.components.SimpleRetrofitCallback;
 import ua.statisticco2app.models.requests.GetStatisticRequest;
 import ua.statisticco2app.models.responses.GetStatisticResponse;
 
-public class SimpleFragment extends BaseFragment {
+public class SimpleFragment extends BaseFragment implements SwipeRefreshLayout.OnRefreshListener {
 
     private static final String TAG = SimpleFragment.class.getSimpleName();
 
@@ -25,6 +26,9 @@ public class SimpleFragment extends BaseFragment {
     @InjectView(R.id.list)
     ListView list;
 
+    @InjectView(R.id.swipeRefreshLayout)
+    SwipeRefreshLayout swipeRefreshLayout;
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_simple_list;
@@ -34,24 +38,41 @@ public class SimpleFragment extends BaseFragment {
     protected void onViewCreated() {
         Log.d(TAG, "onViewCreated");
         list.setAdapter(new IndicationArrayAdapter(getActivity()));
-        StatisticCO2Application.getInstance().getSpiceManager().execute(new GetStatisticRequest(), callback);
+        onRefresh();
     }
 
     @Override
     protected void setListeners() {
-
+        Log.d(TAG, "setListeners");
+        swipeRefreshLayout.setColorSchemeResources(
+                android.R.color.holo_orange_light,
+                android.R.color.holo_blue_light,
+                android.R.color.holo_green_light,
+                android.R.color.holo_red_light);
+        swipeRefreshLayout.setOnRefreshListener(this);
     }
 
     private SimpleRetrofitCallback<GetStatisticResponse> callback = new SimpleRetrofitCallback<GetStatisticResponse>() {
         @Override
         public void onRequestFailure(SpiceException spiceException) {
 //            super.onRequestFailure(spiceException);
+            swipeRefreshLayout.setRefreshing(false);
             Toast.makeText(getActivity(), "failed", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onRequestSuccess(GetStatisticResponse indications) {
-            ((IndicationArrayAdapter)list.getAdapter()).addAll(indications);
+            swipeRefreshLayout.setRefreshing(false);
+            IndicationArrayAdapter adapter = ((IndicationArrayAdapter)list.getAdapter());
+            adapter.clear();
+            adapter.addAll(indications);
+
         }
     };
+
+    @Override
+    public void onRefresh() {
+        Log.d(TAG, "onRefresh");
+        StatisticCO2Application.getInstance().getSpiceManager().execute(new GetStatisticRequest(), callback);
+    }
 }
